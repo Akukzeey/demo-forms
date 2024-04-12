6ec581375be0482fecfa5f37e84381c4de7ff5e5
faroukahmed <thiagoacookie@gmail.com>
2024-04-12 10:34:31 +0300

    Demo Forms

--- app/page.js
+++ app/page.js
@@ -67,21 +67,33 @@ export default function AdministrationForm() {
         <div className="step-indicator">
           Step {currentStep} of 3
         </div>
-        <form name="administration-form" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleFormSubmit} action="/admission-success" data-netlify-success="/admission-success">
-          <input type="hidden" name="form-name" value="administration-form" />
-          <input type="hidden" name="bot-field" />
-          <div className={currentStep !== 1 ? 'hidden' : ''}>
-            <StudentInfo formData={formData.studentInfo} setFormData={setFormData} onNextStep={handleNextStep} />
-          </div>
-          <div className={currentStep !== 2 ? 'hidden' : ''}>
-            <ParentInfo parentFormData={formData.parentInfo} setFormData={setFormData} onNextStep={handleNextStep} onPreviousStep={handlePreviousStep} />
-          </div>
-          {currentStep === 3 && (
-              <div>
-                if you have entered the forms correctly you can move to the next step
-                <button>Submit</button>
-              </div>
-          )}
+        {/*<form name="administration-form" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleFormSubmit} action="/admission-success" data-netlify-success="/admission-success">*/}
+        {/*  <input type="hidden" name="form-name" value="administration-form" />*/}
+        {/*  <input type="hidden" name="bot-field" />*/}
+        {/*  <div className={currentStep !== 1 ? 'hidden' : ''}>*/}
+        {/*    <StudentInfo formData={formData.studentInfo} setFormData={setFormData} onNextStep={handleNextStep} />*/}
+        {/*  </div>*/}
+        {/*  <div className={currentStep !== 2 ? 'hidden' : ''}>*/}
+        {/*    <ParentInfo parentFormData={formData.parentInfo} setFormData={setFormData} onNextStep={handleNextStep} onPreviousStep={handlePreviousStep} />*/}
+        {/*  </div>*/}
+        {/*  {currentStep === 3 && (*/}
+        {/*      <div>*/}
+        {/*        if you have entered the forms correctly you can move to the next step*/}
+        {/*        <button>Submit</button>*/}
+        {/*      </div>*/}
+        {/*  )}*/}
+        {/*</form>*/}
+        <form method='POST' name='contact' data-netlify='true'>
+          <input type="hidden" name="form-name" value="contact"/>
+          <input type="text"/>
+          <p>
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name' name='name'/>
+          </p>
+          <p>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' name='email'/>
+          </p>
         </form>
       </div>
   );
