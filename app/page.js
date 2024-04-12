5ca9aed81f97c97f4136a4a7e4c13a38c590ee48
faroukahmed <thiagoacookie@gmail.com>
2024-04-12 10:53:01 +0300

    Demo Forms

--- app/page.js
+++ app/page.js
@@ -1,103 +1,84 @@
 'use client'
-import { useState } from 'react';
-import StudentInfo from "@/components/StudentForm";
-import ParentInfo from "@/components/ParentForm";
+import { useState, useEffect } from 'react';
 
-export default function AdministrationForm() {
-  const [formData, setFormData] = useState({
-    studentInfo: {
-      firstName: '',
-      middleName: '',
-      lastName: '',
-      emailAddress: '',
-      age: '',
-    },
-    parentInfo: {
-      parent1FullName: '',
-      parent1Relationship: '',
-      parent1LevelOfEducation: '',
-      parent1WorkingStatus: 'working',
-    },
-  });
+export default function RegisterComponent() {
+  const [submissions, setSubmissions] = useState([]);
+  const [emailExists, setEmailExists] = useState(false);
+  const [email, setEmail] = useState('');
 
-  const [currentStep, setCurrentStep] = useState(1);
+  const siteId = process.env.NEXT_PUBLIC_NETLIFY_SITE_ID;
+  const accessToken = process.env.NEXT_PUBLIC_NETLIFY_ACCESS_TOKEN;
 
-  const handleNextStep  = () => {
-    setCurrentStep(currentStep + 1);
-  };
+  useEffect(() => {
+    async function fetchSubmissions() {
+      try {
+        const response = await fetch(`https://api.netlify.com/api/v1/sites/${siteId}/submissions`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setSubmissions(data);
+        } else {
+          console.error('Failed to fetch form submissions:', response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching form submissions:', error);
+      }
+    }
+    fetchSubmissions();
+  }, [siteId, accessToken]);
 
-  const handlePreviousStep = () => {
-    setCurrentStep(currentStep - 1);
-  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
 
-  const handleFormSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    const form = e.target;
+    const emailExists = submissions.some(submission => submission.data['user-email'] === email);
+    setEmailExists(emailExists);
 
-    const combinedFormData = {
-      ...formData.studentInfo,
-      ...formData.parentInfo,
-    };
+    if (!emailExists) {
+      e.target.submit();
 
-    const flattenedFormData = {};
-
-    Object.keys(combinedFormData).forEach(key => {
-      if (Array.isArray(combinedFormData[key])) {
-        flattenedFormData[key] = combinedFormData[key][0];
-      } else {
-        flattenedFormData[key] = combinedFormData[key];
-      }
-    });
-
-    const textArea = document.createElement('textarea');
-    textArea.style.display = 'none';
-    textArea.name = 'formData';
-    textArea.value = JSON.stringify(flattenedFormData, null, 2);
-    form.appendChild(textArea);
-
-      form.submit();
-  };
+      localStorage.setItem('formSubmitted', 'true');
+    } else {
+      // Email already exists, do not submit the form
+      console.log('Email already exists');
+    }
+  }
 
   return (
-      <div id='Administration-form' className='container'>
-        <div className='administation-note-container'>
-          <p className='administration-form-p-tag'>The following form must be completed before your application will be considered. Please complete each item carefully.</p>
+      <div id='Register'>
+        <div className='sign-up-container'>
+          <div className='d-flex align-items-center flex-column mb-lg-4 mb-md-3'>
+            <h1 className='sign-up-header'>Create an Account</h1>
+          </div>
+          <form name="contact v3" data-netlify="true" method='post' onSubmit={handleSubmit} data-netlify-honeypot='bot-field' action="/success" data-netlify-success="/success">
+            <input type='hidden' name='form-name' value='contact v3'/>
+            <div hidden>
+              <input name='bot-field'/>
+            </div>
+            <div className="mb-lg-4 mb-md-3 mb-2">
+              <label htmlFor="your-name" className="form-label">Your Name</label>
+              <input type="text" className="form-control" id="your-name" name='your-name' required/>
+            </div>
+            <div className="mb-lg-4 mb-md-3 mb-2">
+              <label htmlFor="email" className="form-label">Your Email</label>
+              <input type="email" className="form-control" id="email" name='user-email' value={email} onChange={handleEmailChange} required/>
+              {emailExists && <p style={{ color: 'red' }}>This email already exists. Please use a different email.</p>}
+            </div>
+            <div className='mb-lg-4 mb-md-3 mb-2'>
+              <label htmlFor="phone-number" className="form-label">Number</label>
+              <input type="tel" className="form-control" id="phone-number" name="phone-number" required/>
+            </div>
+            <div className='d-flex justify-content-center'>
+              <input type="submit" className='btn sign-up-btn' value='Sign Up'/>
+            </div>
+          </form>
         </div>
-        <div className="step-indicator">
-          Step {currentStep} of 3
-        </div>
-        {/*<form name="administration-form" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleFormSubmit} action="/admission-success" data-netlify-success="/admission-success">*/}
-        {/*  <input type="hidden" name="form-name" value="administration-form" />*/}
-        {/*  <input type="hidden" name="bot-field" />*/}
-        {/*  <div className={currentStep !== 1 ? 'hidden' : ''}>*/}
-        {/*    <StudentInfo formData={formData.studentInfo} setFormData={setFormData} onNextStep={handleNextStep} />*/}
-        {/*  </div>*/}
-        {/*  <div className={currentStep !== 2 ? 'hidden' : ''}>*/}
-        {/*    <ParentInfo parentFormData={formData.parentInfo} setFormData={setFormData} onNextStep={handleNextStep} onPreviousStep={handlePreviousStep} />*/}
-        {/*  </div>*/}
-        {/*  {currentStep === 3 && (*/}
-        {/*      <div>*/}
-        {/*        if you have entered the forms correctly you can move to the next step*/}
-        {/*        <button>Submit</button>*/}
-        {/*      </div>*/}
-        {/*  )}*/}
-        {/*</form>*/}
-        <form method='POST' name='contact' data-netlify='true'>
-          <input type="hidden" name="form-name" value="contact"/>
-          <input type="text"/>
-          <p>
-            <label htmlFor="name">Name</label>
-            <input type="text" id='name' name='name'/>
-          </p>
-          <p>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' name='email'/>
-          </p>
-          <button type='submit'>
-            submit
-          </button>
-        </form>
       </div>
   );
 }
