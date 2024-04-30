22b2f284973d0c1b9be541403877fc2516d5f01e
faroukahmed <thiagoacookie@gmail.com>
2024-04-30 11:37:14 +0300

    Demo Forms

--- app/administration/page.js
+++ app/administration/page.js
@@ -14,8 +14,9 @@ const Form = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        console.log('Form submitted:', formData);
+        setFormData({ name: '', email: '', message: '' });
     };
-
     return (
         <form name="contactForm" data-netlify="true" onSubmit={handleSubmit}>
             <input type='hidden' name='form-name' value='contactForm'/>
@@ -23,34 +24,11 @@ const Form = () => {
                 <input name='bot-field'/>
             </div>
             <label htmlFor="name">Name:</label>
-            <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-            />
-
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
             <label htmlFor="email">Email:</label>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-            />
-
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
             <label htmlFor="message">Message:</label>
-            <textarea
-                id="message"
-                name="message"
-                value={formData.email}
-                onChange={handleChange}
-                required
-            />
-
+            <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
             <button type="submit">Submit</button>
         </form>
     );
